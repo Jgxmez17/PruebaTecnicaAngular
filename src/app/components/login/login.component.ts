8af8618e27ba3e7bcb171d3e08537e3ae1780a91
jgomez-17 <jossygomez509@gmail.com>
2023-03-18 12:09:22 -0500

    registro y login terminado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,14 +11,20 @@ export class LoginComponent {
   email: string | undefined;
   password: string | undefined;
 
-  constructor(public usersService: UsersService) {}
+  constructor(public usersService: UsersService, public router: Router) {}
 
   login() {
     const user = {email: this.email, password: this.password};
     this.usersService.login(user).subscribe( data => {
       this.usersService.setToken(data.token);
+      this.router.navigateByUrl('/dashboard');
       // console.log(data);
+    },
+    
+    error => {
+      console.log(error);
     });
+    
     // console.log(this.email);
     // console.log(this.password);
   }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -12,12 +13,13 @@ export class RegisterComponent {
   confirmPassword: string | undefined;
   passwordError: boolean | undefined;
 
-  constructor(public usersService: UsersService) {}
+  constructor(public usersService: UsersService, public router: Router) {}
 
   register() {
     const user = {email: this.email, password: this.password};
     this.usersService.register(user).subscribe(data => {
       this.usersService.setToken(data.token);
+      this.router.navigateByUrl('/dashboard');
       // console.log(data);
     });
     // console.log(this.email);
